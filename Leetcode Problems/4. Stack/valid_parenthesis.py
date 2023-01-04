389daca6f293bd00e53eb9d74fd452c994404866
Mohammed Jawwadul Islam Fida <fidajisa@hotmail.com>
2023-01-04 22:19:07 +0600

    valid parenthesis done

--- Leetcode Problems/4. Stack/valid_parenthesis.py
+++ Leetcode Problems/4. Stack/valid_parenthesis.py
@@ -4,6 +4,26 @@ Leetcode: 20. Valid Parentheses
 https://leetcode.com/problems/valid-parentheses/
 """
 
-
+    
 class Solution:
+    
+    def is_empty(self, st):
+        return len(st) == 0
+
     def isValid(self, s: str) -> bool:
+   
+        st = []
+        bracket_arr = ["(", "{", "["]
+        for c in s:
+            if c in bracket_arr:
+                st.append(c)
+        
+            elif c == ')' and not(self.is_empty(st)) and st[-1] == '(':
+                st.pop()
+            elif c == '}' and not(self.is_empty(st)) and st[-1] == '{':
+                st.pop()
+            elif c == ']' and not(self.is_empty(st)) and st[-1] == '[':
+                st.pop()
+            else:
+                return False
+        return self.is_empty(st)
